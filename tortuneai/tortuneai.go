753ed7b1d93beffcb3a0bfc0e89595b4f4d06770
Daniel Stamer <stamer@google.com>
2023-08-10 05:27:32 +0000

    add comments and fix typos in prompt
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- tortuneai/tortuneai.go
+++ tortuneai/tortuneai.go
@@ -12,16 +12,23 @@ import (
 
 const Version = "0.0.1"
 
+// HitMe proxies a text prompt to Google Cloud's text-bison model hosted on
+// Vertex AI and returns the generated response from the model. If no prompt
+// is passed to the function, it will default to a prompt that is engineered
+// to generate a terrible dad joke.
 func HitMe(prompt string, project string) (string, error) {
 	if project == "" {
 		return "", fmt.Errorf("no project ID set")
 	}
 
+	// override empty prompt if not specified by user
 	if prompt == "" {
-		prompt = "tell me a terribe dad joke about tech"
+		prompt = "tell me a terrible dad joke about tech"
 	}
 
 	ctx := context.Background()
+	// create Vertex AI client, but override with us-central1, due to
+	// availability of hosted model
 	client, err := aiplatform.NewPredictionClient(
 		ctx,
 		option.WithEndpoint("us-central1-aiplatform.googleapis.com:443"),
@@ -31,6 +38,7 @@ func HitMe(prompt string, project string) (string, error) {
 	}
 	defer client.Close()
 
+	// use default parameters, as found in the web console of Vertex AI
 	parameters, err := structpb.NewValue(map[string]interface{}{
 		"temperature":     0.2,
 		"maxOutputTokens": 256,
@@ -41,15 +49,18 @@ func HitMe(prompt string, project string) (string, error) {
 		return "", err
 	}
 
+	// this will only need the actual prompt
 	instances, err := structpb.NewValue(map[string]interface{}{
 		"prompt": prompt,
 	})
 
+	// construct the full resource name of the hosted model
 	endpoint := fmt.Sprintf(
 		"projects/%s/locations/us-central1/publishers/google/models/text-bison",
 		project,
 	)
 
+	// instantiate the request object and execute it
 	req := &aiplatformpb.PredictRequest{
 		Endpoint:   endpoint,
 		Instances:  []*structpb.Value{instances},
@@ -60,7 +71,7 @@ func HitMe(prompt string, project string) (string, error) {
 		return "", err
 	}
 
+	// extract model response and return it
 	predictionFields := resp.Predictions[0].GetStructValue().AsMap()
-
 	return predictionFields["content"].(string), nil
 }
