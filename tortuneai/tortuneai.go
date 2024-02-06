c914edb7ba087d57a493385e5dba65ae152b1525
Daniel Stamer <stamer@google.com>
2024-02-06 18:17:20 +0000

    switch to gemini
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- tortuneai/tortuneai.go
+++ tortuneai/tortuneai.go
@@ -4,15 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	aiplatform "cloud.google.com/go/aiplatform/apiv1"
-	"cloud.google.com/go/aiplatform/apiv1/aiplatformpb"
-	"google.golang.org/api/option"
-	"google.golang.org/protobuf/types/known/structpb"
+	"cloud.google.com/go/vertexai/genai"
 )
 
-const Version = "0.0.1"
+const Version = "0.0.2"
 
-// HitMe proxies a text prompt to Google Cloud's text-bison model hosted on
+// HitMe proxies a text prompt to Google Cloud's gemini-pro model hosted on
 // Vertex AI and returns the generated response from the model. If no prompt
 // is passed to the function, it will default to a prompt that is engineered
 // to generate a terrible dad joke.
@@ -27,51 +24,24 @@ func HitMe(prompt string, project string) (string, error) {
 	}
 
 	ctx := context.Background()
-	// create Vertex AI client, but override with us-central1, due to
-	// availability of hosted model
-	client, err := aiplatform.NewPredictionClient(
+	region := "us-central1"
+
+	client, err := genai.NewClient(
 		ctx,
-		option.WithEndpoint("us-central1-aiplatform.googleapis.com:443"),
-	)
-	if err != nil {
-		return "", err
-	}
-	defer client.Close()
-
-	// use default parameters, as found in the web console of Vertex AI
-	parameters, err := structpb.NewValue(map[string]interface{}{
-		"temperature":     0.2,
-		"maxOutputTokens": 256,
-		"topK":            40,
-		"topP":            0.95,
-	})
-	if err != nil {
-		return "", err
-	}
-
-	// this will only need the actual prompt
-	instances, err := structpb.NewValue(map[string]interface{}{
-		"prompt": prompt,
-	})
-
-	// construct the full resource name of the hosted model
-	endpoint := fmt.Sprintf(
-		"projects/%s/locations/us-central1/publishers/google/models/text-bison",
 		project,
+		region,
 	)
 
-	// instantiate the request object and execute it
-	req := &aiplatformpb.PredictRequest{
-		Endpoint:   endpoint,
-		Instances:  []*structpb.Value{instances},
-		Parameters: parameters,
-	}
-	resp, err := client.Predict(ctx, req)
 	if err != nil {
 		return "", err
 	}
 
-	// extract model response and return it
-	predictionFields := resp.Predictions[0].GetStructValue().AsMap()
-	return predictionFields["content"].(string), nil
+	model := client.GenerativeModel("gemini-pro")
+
+	response, err := model.GenerateContent(ctx, genai.Text(prompt))
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%v", response.Candidates[0].Content.Parts[0]), nil
 }
